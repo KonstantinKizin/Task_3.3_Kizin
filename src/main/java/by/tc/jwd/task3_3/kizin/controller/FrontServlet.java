3bcf26f6d3421515772819365b85f470df7db087
konstantin Kizin <KostyKizingmail.com>
2017-12-01 00:54:34 +0300

    version with bad pagination

--- src/main/java/by/tc/jwd/task3_3/kizin/controller/FrontServlet.java
+++ src/main/java/by/tc/jwd/task3_3/kizin/controller/FrontServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,9 +22,9 @@ public class FrontServlet extends HttpServlet {
     private CommandProvider producer = new CommandProvider();
     private final String HIDDEN_PARAMETER = "command";
     private final String EMPLOYEES_PAGE = "/WEB-INF/jsp/employees.jsp";
-    private final String ATTR_NAME = "employeeList";
+    private final String LIST_ATTR_NAME = "employeeList";
     private final String ERROR_PAGE = "/error.jsp";
-
+    private static int noOfRecords;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -38,7 +39,17 @@ public class FrontServlet extends HttpServlet {
             String commandName = request.getParameter(HIDDEN_PARAMETER);
             Command command = producer.getCommandMap().get(commandName);
             List<Employee> employeeList = command.execute();
-            request.setAttribute(ATTR_NAME,employeeList);
+            int page = 1;
+            int recordsPerPage = 3;
+            noOfRecords = employeeList.size() - 3;
+            if(request.getParameter("page") != null){
+                page = Integer.parseInt(request.getParameter("page"));
+            }
+            List<Employee> responseList = getPartOfEmployee(employeeList,(page-1)*recordsPerPage,recordsPerPage);
+            int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
+            request.setAttribute(LIST_ATTR_NAME,responseList);
+            request.setAttribute("noOfPages", noOfPages);
+            request.setAttribute("currentPage", page);
             RequestDispatcher rd = getServletContext().getRequestDispatcher(EMPLOYEES_PAGE);
             rd.forward(request,response);
             employeeList.clear();
@@ -57,5 +68,23 @@ public class FrontServlet extends HttpServlet {
             throw new RuntimeException(new ControllerException(e));
         }
     }
+
+
+
+
+    public List<Employee> getPartOfEmployee(List<Employee> list , int start , int count){
+
+        List<Employee> employeePart = new ArrayList<>();
+
+        int index = 0;
+
+        for(int i = start; i < (start + count); i++  ){
+            employeePart.add(list.get(i));
+            index++;
+        }
+        noOfRecords = list.size() - index;
+        return employeePart;
+    }
+
 
 }
--- src/main/webapp/WEB-INF/jsp/employees.jsp
+++ src/main/webapp/WEB-INF/jsp/employees.jsp
@@ -1,3 +1,6 @@
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.Arrays" %>
+<%@ page import="by.tc.jwd.task3_3.kizin.entity.Employee" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
@@ -30,6 +33,12 @@
 
 </div>
 
+<c:if test="${currentPage != 1}">
+    <td><a href="/frontController?command=sax&page=${currentPage - 1}">Previous</a></td>
+</c:if>
+
+<%--For displaying Page numbers.
+The when condition does not display a link for the current page--%>
 <table border="1" cellpadding="5" cellspacing="5">
     <tr>
         <c:forEach begin="1" end="${noOfPages}" var="i">
@@ -38,46 +47,19 @@
                     <td>${i}</td>
                 </c:when>
                 <c:otherwise>
-                    <td><a href="/frontController?page=${i}">${i}</a></td>
+                    <td><a href="/frontController?command=sax&page=${i}">${i}</a></td>
                 </c:otherwise>
             </c:choose>
         </c:forEach>
     </tr>
 </table>
 
-Text
-<br>
-<c:set var="count" value="${0}" scope="page" />
-<c:set var="start" value="${0}" scope="page" />
-<c:set var="startcount" value="${param.start}" scope="page" />
-
-<c:set var="endcount" value="${param.end}" scope="page" />
-<c:set var="allend" value="${72}" scope="page" />
-<c:set var="stepcount" value="${1}" scope="page" />
-
-<c:if test="${endcount>allend}">
-    <c:set var="endcount" value="${allend}" scope="page" />
-</c:if>
-<c:forEach var="counter" begin="${startcount}" end="${endcount}" step="${stepcount}">
-    <c:if test="${count < 24}">
-        ${counter}<br/>
-        <c:set var="count" value="${count + 1}" scope="page" />
-        <c:set var="end" value="${counter+count-1}" scope="page" />
-        <c:set var="start" value="${counter}" scope="page" />
-    </c:if>
-</c:forEach>
-<c:if test="${endcount>allend}">
-    <c:set var="start" value="${1}" scope="page" />
-    <c:set var="end" value="${allend}" scope="page" />
+<%--For displaying Next link --%>
+<c:if test="${currentPage lt noOfPages}">
+    <td><a href="/frontController?command=sax&page=${currentPage + 1}">Next</a></td>
 </c:if>
 
 
-<c:if test="${endcount!=allend}">
-    <a href="pagination.jsp?start=${(count==1)?1:start}&end=${end}">NEXT</a>&nbsp;&nbsp;&nbsp;
-</c:if>
-<c:if test="${startcount>24-1}">
-    <a href="pagination.jsp?start=${startcount-24+1}&end=${startcount}">PREVIOUS</a>
-</c:if>
 
 
 
